a82e54a032f8b3a7ae35aa7204df5b05d764b4cf
Carl Hüffmeier <carlhueffmeier@gmail.com>
2017-11-13 06:30:42 +0100

    refactor: Change function declaration to match style

--- frontend/src/containers/Root/RootContainer.js
+++ frontend/src/containers/Root/RootContainer.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import Login from 'containers/Login';
 
-const RootContainer = ({ store }) => (
-  <Provider store={store}>
-    <div className="app">
-      <h1>Readable</h1>
-      <Login />
-    </div>
-  </Provider>
-);
-
 RootContainer.propTypes = {
   store: PropTypes.object.isRequired
 };
 
-export default RootContainer;
+export default function RootContainer({ store }) {
+  return (
+    <Provider store={store}>
+      <div className="app">
+        <h1>Readable</h1>
+        <Login />
+      </div>
+    </Provider>
+  );
+}
