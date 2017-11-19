ae0622386132d38dc8bfc726ef27dd1eda1b01e1
Carl Hüffmeier <carlhueffmeier@gmail.com>
2017-11-19 16:40:47 +0100

    fix: Bug caused by wrong import

--- frontend/src/components/SignInAndOutButton/SignInAndOutButton.js
+++ frontend/src/components/SignInAndOutButton/SignInAndOutButton.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SignInButton from 'containers/SignInButton';
-import SignOutButton from 'components/SignOutButton';
+import SignOutButton from 'containers/SignOutButton';
 import SpinningButton from 'components/SpinningButton';
 
 SignInAndOutButton.propTypes = {
   isFetching: PropTypes.bool.isRequired,
   isAuthed: PropTypes.bool.isRequired
 };
+
 export default function SignInAndOutButton({ isAuthed, isFetching }) {
   if (isFetching) {
     return <SpinningButton className="btn btn-sm" />;
